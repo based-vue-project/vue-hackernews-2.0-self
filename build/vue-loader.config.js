2d16af95e87af5ea8f927fd2e77dff959f663538
fen666 <lf00817@163.com>
2018-04-12 11:00:07 +0800

    保持原始项目，删除配置文件，基于poro服务器代理后，设置命令行代理，成功运行项目

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,15 +1,12 @@
 /*
  * 独立出vue-loader的配置以便根据环境改变
  */
-var px2rem = require('postcsss-px2rem')
 module.exports = {
   extractCSS: process.env.NODE_ENV === 'production',
-  preserveWhitespace: false, // 去掉元素间空格，减小文件体积
+  preserveWhitespace: false,
   postcss: [
-  	// 使用vue-cli构建的开发环境，autoprefixer没有处理import 和 require 的css文件
     require('autoprefixer')({
       browsers: ['last 3 versions']
-    }),
-    px2rem({remUnit: 100})
+    })
   ]
 }
